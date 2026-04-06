680672d9386c8b0741b055dd6b64e004101faaf7
Farhan <farhanaliforwardsports@gmail.com>
2026-04-06 16:28:07 +0500

    Kafka Events Working. Event Sent/Fired in employee service as Producer.

--- src/main/java/com/example/employee_service/repsitory/EmployeeRepository.java
+++ src/main/java/com/example/employee_service/repsitory/EmployeeRepository.java
@@ -4,4 +4,5 @@ import com.example.employee_service.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+     boolean existsByFirstName(String firstName);
 }
--- src/main/java/com/example/employee_service/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/employee_service/service/impl/EmployeeServiceImpl.java
@@ -1,23 +1,32 @@
 package com.example.employee_service.service.impl;
 
+import com.example.employee_service.dto.event.EmployeeCreatedEvent;
 import com.example.employee_service.dto.request.EmployeeRequest;
 import com.example.employee_service.dto.response.EmployeeResponse;
+import com.example.employee_service.events.EmployeeKafkaProducer;
+import com.example.employee_service.exception.ValidationException;
 import com.example.employee_service.mapper.EmployeeMapper;
 import com.example.employee_service.repsitory.EmployeeRepository;
 import com.example.employee_service.service.EmployeeService;
 import com.example.employee_service.service.MasterDataValidationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
     private final EmployeeMapper employeeMapper;
     private final MasterDataValidationService masterDataValidationService;
+
+    // event working
+    private final EmployeeKafkaProducer employeeKafkaProducer;
+
 
     @Override
     public List<EmployeeResponse> getAllEmployees() {
@@ -33,11 +42,24 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public EmployeeResponse createEmployee(EmployeeRequest employeeRequest) {
 
+        if (employeeRepository.existsByFirstName(employeeRequest.firstName())) {
+            throw new ValidationException("Employee with the same first name already exists");
+        }
         // validate the qualification exist by id in master data.
         masterDataValidationService.validateDepartmentById(employeeRequest.qualificationId());
 
         var newEmployee = employeeMapper.toEntity(employeeRequest);
         employeeRepository.save(newEmployee);
+
+        // create & fire event.
+        // 🔥 EVENT FIRE KARO
+        EmployeeCreatedEvent event = new EmployeeCreatedEvent(
+                newEmployee.getId(),
+                newEmployee.getFirstName()
+        );
+
+        employeeKafkaProducer.sendEmployeeCreatedEvent(event);
+        log.info("🔥 EVENT Sent to Consumer: {}", event);
         return employeeMapper.toEmployeeResponse(newEmployee);
     }
 
